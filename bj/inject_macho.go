6bd3f176426e39a9863f5677862c47bc4e065687
awgh <awgh@awgh.org>
2019-01-07 20:53:28 -0800

    Fixed Mach-O code cave detection according to the scrolls: http://secureallthethings.blogspot.com/2014/08/patching-mach-o-format-simple-and-easy.html
    Let us never speak of this again.

--- bj/inject_macho.go
+++ bj/inject_macho.go
@@ -1,9 +1,7 @@
 package bj
 
 import (
-	"bytes"
 	"debug/macho"
-	"encoding/binary"
 	"log"
 )
 
@@ -17,66 +15,16 @@ func MachoBinject(sourceFile string, destFile string, shellcode string, config *
 	if err != nil {
 		return err
 	}
-	for _, command := range machoFile.Loads {
-		rawCommand := command.Raw()
-		log.Printf("LoadCmd of command is: %x\n", rawCommand[0])
-		if rawCommand[0] == 0x1 {
-			log.Printf("32 bit LoadCmdSegment\n")
-			mseg32 := macho.Segment32{}
-			buf := bytes.NewBuffer(rawCommand)
-			err := binary.Read(buf, binary.LittleEndian, &mseg32)
-			if err != nil {
-				return err
-			}
-			log.Printf("Your struct sir: %+v\n", mseg32)
-		}
-		if rawCommand[0] == byte(macho.LoadCmdSegment64) {
-			log.Printf("64 bit LoadCmdSegment\n")
-			mseg64 := macho.Segment64{}
-			buf := bytes.NewBuffer(rawCommand)
-			err := binary.Read(buf, binary.LittleEndian, &mseg64)
-			if err != nil {
-				return err
-			}
-			log.Printf("Your struct sir: %+v\n", mseg64)
-			log.Printf("Your CmdLen is : %d\n", mseg64.Len)
-			log.Printf("Vs RawCmd Len: %d\n", len(rawCommand))
-			//msegHeader := mseg64.
-			//endCmd := mseg64.Offset + uint64(mseg64.Len)
-			//fmt.Printf("Your End Cmd is at: %+v\n", endCmd)
-		}
-	}
-
 	for _, section := range machoFile.Sections {
-		// Only parse text sections
-		if section.SectionHeader.Seg == "__TEXT" {
-			log.Printf("Section looks like: %+v\n", section)
-			// Calculate end of sections
-			sectEnd := section.SectionHeader.Size + uint64(section.SectionHeader.Offset)
-			log.Printf("Section length is: %v\n", section.SectionHeader.Size)
-			log.Printf("Section end is at: %v\n", sectEnd)
-			// Parse section data for last cmd
-			//sectionData, err := section.Data()
-			//if err != nil {
-			//	return err
-			//}
-
-			//fmt.Printf("Section structure looks like: %+v\n", sectionData)
-
+		if section.SectionHeader.Seg == "__TEXT" && section.Name == "__text" {
+			caveOffset := 0x20 /* magic value */ + machoFile.FileHeader.Cmdsz
+			log.Printf("Code Cave Size: %x - %x = %x\n", section.Offset, caveOffset, section.Offset-caveOffset)
 		}
-		//sectionData, err := section.Data()
-		//if err != nil {
-		//	return err
-		//}
-		//firstHeader := sectionData[:3]
-		//fmt.Printf("First four bytes: %+v\n", firstHeader)
-
-		//	if cave.Start >= uint64(section.Offset) && cave.End <= (section.Size+uint64(section.Offset)) &&
-		//	header['MagicNumber'] = hex(struct.unpack("<I", self.bin.read(4))[0])
-
-		//	cave.End-cave.Start >= uint64(MIN_CAVE_SIZE) {
-		//	log.Printf("Cave found (start/end/size): %d / %d / %d \n", cave.Start, cave.End, cave.End-cave.Start)
 	}
+
+	//
+	// END CODE CAVE DETECTION SECTION
+	//
 
 	return nil
 }
