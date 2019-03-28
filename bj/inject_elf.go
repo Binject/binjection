2e48f0b187cb3d37634b88032a87806758d4dba5
awgh <awgh@awgh.org>
2019-03-27 20:41:02 -0700

    Partial implementation of DYN injector, not functional, just checking in for safety.

--- bj/inject_elf.go
+++ bj/inject_elf.go
@@ -1,6 +1,7 @@
 package bj
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -210,6 +211,7 @@ func NoteToLoad(elfFile *elf.File, destFile string, userShellCode []byte, fsize 
 }
 
 func dynamicMethod(elfFile *elf.File, destFile string, userShellCode []byte) error {
+	// from positron/elfhack:
 	// The injected code needs to be executed before any init code in the
 	// binary. There are three possible cases:
 	// - The binary has no init code at all. In this case, we will add a
@@ -224,7 +226,74 @@ func dynamicMethod(elfFile *elf.File, destFile string, userShellCode []byte) err
 	// The binary may have .ctors instead of DT_INIT_ARRAY, for its init
 	// functions, but this falls into the second case above, since .ctors
 	// are actually run by DT_INIT code.
-	// from positron/elfhack
+
+	log.Println("Entering Dynamic Method")
+
+	// count DT_INITs, DT_INIT_ARRAYs, and find one NULL
+	var initCnt, arrayCnt int
+	originalEntryPoint := -1
+	nullIdx := -1
+	for idx, tv := range elfFile.DynTags {
+		switch tv.Tag {
+		case elf.DT_INIT:
+			initCnt++
+			originalEntryPoint = int(tv.Value)
+		case elf.DT_INIT_ARRAY:
+			arrayCnt++
+			//todo: originalEntryPoint = tv.Value
+		case elf.DT_NULL:
+			if nullIdx < 0 {
+				nullIdx = idx
+			}
+		}
+	}
+
+	// Insert the payload
+	scAddr := uint64(0)
+	sclen := uint64(0)
+	shellcode := []byte{}
+	for _, p := range elfFile.Progs {
+		if p.Type == elf.PT_LOAD && p.Flags == (elf.PF_R|elf.PF_X) {
+			scAddr = p.Vaddr + p.Filesz
+			if originalEntryPoint > 0 {
+				shellcode = api.ApplySuffixJmpIntel64(userShellCode, uint32(scAddr), uint32(originalEntryPoint), elfFile.ByteOrder)
+			} else {
+				shellcode = userShellCode
+			}
+			sclen = uint64(len(shellcode))
+			log.Println("Shellcode Length: ", sclen)
+			p.Filesz += sclen
+			p.Memsz += sclen
+		}
+	}
+	sortedSections := elfFile.Sections[:]
+	sort.Slice(sortedSections, func(a, b int) bool { return elfFile.Sections[a].Offset < elfFile.Sections[b].Offset })
+	for _, s := range sortedSections {
+		if s.Size+s.Addr == scAddr {
+			s.Size += sclen
+		}
+	}
+
+	// - The binary has no init code at all. In this case, we will add a
+	//   DT_INIT entry pointing to the injected code.
+	if initCnt == 0 && arrayCnt == 0 {
+		if nullIdx < 0 {
+			return errors.New("No init in a DYN and no free slots means an invalid source binary")
+		}
+		elfFile.DynTags[nullIdx] = elf.DynTagValue{Tag: elf.DT_INIT, Value: scAddr}
+	} else if initCnt > 0 {
+		// - The binary has a DT_INIT entry. In this case, we will interpose:
+		//   we change DT_INIT to point to the injected code, and have the
+		//   injected code call the original DT_INIT entry point.
+		for idx, tv := range elfFile.DynTags {
+			switch tv.Tag {
+			case elf.DT_INIT:
+				elfFile.DynTags[idx] = elf.DynTagValue{Tag: elf.DT_INIT, Value: scAddr}
+			}
+		}
+	}
+
+	elfFile.Insertion = userShellCode
 
 	return nil
 }
